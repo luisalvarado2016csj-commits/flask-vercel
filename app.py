e871711fef024ab9893f916cd8e936cf7d9b63b6
ArturoAP <luisalvarado2016@gmail.com>
2026-05-24 20:12:25 -0500

    Update: todo en la raiz

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Tu base de datos simulada
 ALUMNOS_DB = [
     {"id": "20240001", "nombres": "Arturo Alejandro", "curso": "Programación Aplicada II", "ciclo": "V"},
     {"id": "20240002", "nombres": "Denzel Omar", "curso": "Programación Aplicada II", "ciclo": "V"}
 ]
 
-# AHORA LA RAÍZ BOTARÁ TODO EN UNA SOLA PANTALLA
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({
